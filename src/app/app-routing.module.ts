c8db391852bcbfb9acf7c5da06173e662afebf34
unknown <devansh.bali@gmail.com>
2019-10-16 20:36:39 +0530

    adding files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductlistComponent } from './products/productlist.component';
+
+const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'products', component: ProductlistComponent},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductlistComponent } from './products/productlist.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, WelcomeComponent, ProductlistComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
